e46d22c8b5f6a80e9be4ef081166c22758574ee1
2024tm93706 <2024tm93706@wilp.bits-pilani.ac.in>
2026-04-30 16:16:40 +0530

    feat: update home route message and add blue and green Kubernetes deployment configurations

--- app/app.py
+++ app/app.py
@@ -159,7 +159,7 @@ Protein: 120g/day""",
 
 @app.route("/")
 def home():
-    return jsonify({"message": "ACEest Gym API running"})
+    return jsonify({"message": "ACEest Gym API v1 running"})
 
 @app.route("/programs")
 def get_programs():
